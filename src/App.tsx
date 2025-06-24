ec132e3eaad11c8f768a7dbdc8c3b44f2bf5cf78
olivier <texmex430@gmail.com>
2025-06-24 22:20:52 +0200

    send email

--- src/App.tsx
+++ src/App.tsx
@@ -790,7 +790,7 @@ function App() {
                 {isSubmitting ? (
                   <>
                     <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white"></div>
-                    Envoi en cours...
+                    Envoi en cours....
                   </>
                 ) : (
                   <>
